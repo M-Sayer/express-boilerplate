f50704484bc3ade8d381d887fad7b148e3b04a24
M-Sayer <haji.san@icloud.com>
2020-07-09 12:02:07 -0400

    added error handler middleware

--- src/app.js
+++ src/app.js
@@ -20,4 +20,15 @@ app.get('/', (req, res) => {
     .send('Hello, world!');
 });
 
+app.use(function errorHandler(error, req, res, _next) {
+  let response;
+  if(process.env.NODE_ENV === 'production') {
+    response = { error: {message: 'server error'}};
+  } else {
+    console.log(error);
+    response = { message: error.message, error};
+  }
+  res.status(500).json(response);
+});
+
 module.exports = app;
